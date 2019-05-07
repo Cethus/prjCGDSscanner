236cd9686e526cf59388a84b4f0b5504ff4606b2
Peckins <messias.morais@etec.sp.gov.br>
2019-05-08 05:50:42 +0900

    Project's Initial Commit

---
+++ www/components/barcode.js
@@ -0,0 +1,13 @@
+// This is a JavaScript file
+
+function scanBarcode() {
+  window.plugins.barcodeScanner.scan( function(result) {
+    alert("We got a barcode\n" +
+          "Result: " + result.text + "\n" +
+          "Format: " + result.format + "\n" +
+          "Cancelled: " + result.cancelled);
+  }, 
+  function(error) {
+    alert("Scanning failed: " + error);
+  });
+}
